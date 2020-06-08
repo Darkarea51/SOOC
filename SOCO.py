bf03f613305dc50a586d74d85de6fe2027c38e0e
Carson <46657662+Darkarea51@users.noreply.github.com>
2020-06-08 15:15:11 -0700

    Create SOCO.py

---
+++ SOCO.py
@@ -0,0 +1,32 @@
+#Developed by Carson Wolfe
+#June 8th 2020
+
+from soco import SoCo
+import RPi.GPIO as GPIO
+for time import sleep
+
+speakers = SoCo("192.168.1.135"), SoCo("192.168.1.15")
+
+GPIO.setmode(GPIO.BCM)
+sleepTime = .1
+lightPin = 4
+button = 17
+
+
+
+
+
+    
+
+
+
+
+print(speakers)
+
+
+
+
+
+
+
+
